41a148781a7cf29e4573cf810f27deb1e17a38cc
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-17 16:38:51 +0100

    Added docstring and coomments to the resource

--- resources/Diagnosis.py
+++ resources/Diagnosis.py
@@ -2,24 +2,35 @@ from flask import jsonify, request
 from flask_restful import Resource
 from Model import db, Diagnosis, DiagnosisSchema
 
+# create the schemas
 diagnose_many_schema = DiagnosisSchema(many=True)
 diagnosis_schema = DiagnosisSchema()
 
 
 class DiagnosisResource(Resource):
     def get(self):
+        """
+        Handles get requests
+        """
+        # get all diagnosis from the db
         diagnosis = Diagnosis.query.all()
         diagnosis = diagnose_many_schema.dump(diagnosis).data
+
         return {"status": "success", "data": diagnosis}, 200
 
     def post(self):
+        """
+        Handles post requests
+        """
         json_data = request.get_json(force=True)
 
+        # if request body is empty
         if not json_data:
             return {'message': 'No input data provided'}, 400
 
         data, errors = diagnosis_schema.load(json_data)
 
+        # if error loading the data
         if errors:
             return {"status": "error", "data": errors}, 422
 
@@ -40,13 +51,18 @@ class DiagnosisResource(Resource):
         return {'status': "success", 'data': result}, 201
 
     def put(self):
+        """
+        Handles put requests
+        """
         json_data = request.get_json(force=True)
 
+        # if request body is empty
         if not json_data:
             return {'message': 'No input data provided'}, 400
 
         data, errors = diagnosis_schema.load(json_data)
 
+        # if error loading the data
         if errors:
             return errors, 422
         print("******", data)
@@ -69,6 +85,9 @@ class DiagnosisResource(Resource):
         return {"status": 'success', 'data': result}, 200
 
     def delete(self):
+        """
+        Handles delete requests
+        """
         json_data = request.get_json(force=True)
         if not json_data:
             return {'message': 'No input data provided'}, 400
